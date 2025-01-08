b134e226fb5e58459503950fb1953d9e6e0c076e
Stefan Poindl <stefan@tohuwabohu.io>
2025-01-08 19:25:59 +0100

    follow default tauri project setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 import purgeCss from 'vite-plugin-tailwind-purgecss';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
@@ -22,6 +24,27 @@ export default defineConfig({
 			}
 		})
 	],
+   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 	test: {
 		setupFiles: ['./tests/__mocks__/skeletonProxy.ts']
 	}
