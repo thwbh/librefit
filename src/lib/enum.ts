4f145577cf87ab1436002925c34c9df9cf2991d2
Stefan Poindl <stefan@tohuwabohu.io>
2025-01-11 21:53:25 +0100

    fix bmi category display in wizard result component

--- src/lib/enum.ts
+++ src/lib/enum.ts
@@ -1,23 +1,23 @@
 import { BmiCategory } from '$lib/model';
 
 export enum DataViews {
-	Week = 'WEEK',
-	Month = 'MONTH',
-	Year = 'YEAR'
+  Week = 'WEEK',
+  Month = 'MONTH',
+  Year = 'YEAR'
 }
 
 export enum WizardOptions {
-	Default = 'DEFAULT',
-	Recommended = 'RECOMMENDED',
-	Custom_weight = 'CUSTOM_WEIGHT',
-	Custom_date = 'CUSTOM_DATE',
-	Custom = 'CUSTOM'
+  Default = 'DEFAULT',
+  Recommended = 'RECOMMENDED',
+  Custom_weight = 'CUSTOM_WEIGHT',
+  Custom_date = 'CUSTOM_DATE',
+  Custom = 'CUSTOM'
 }
 
 export function enumKeys(obj: {}) {
-	return Object.keys(obj).filter((k) => Number.isNaN(+k));
+  return Object.keys(obj).filter((k) => Number.isNaN(+k));
 }
 
 export const getBmiCategoryDisplayValue = (bmiCategory: BmiCategory) => {
-	return bmiCategory.toLowerCase().replace('_', ' ');
+  return bmiCategory.split(/(?=[A-Z])/).join(' ').toLowerCase();
 };
