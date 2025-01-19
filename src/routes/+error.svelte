06bbce3050972b84d24cd7a4af5066b10c78c3e9
Stefan Poindl <stefan@tohuwabohu.io>
2025-01-19 22:41:03 +0100

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-	import { preventDefault } from 'svelte/legacy';
-
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import ErrorBot from '$lib/assets/icons/error-bot.svg?component';
 	import { goto } from '$app/navigation';
 </script>
 
 <section>
 	<div class="container mx-auto p-8 space-y-8">
-		<h1>{$page.status}: {$page.error.message}</h1>
+		<h1>{page.status}: {page.error.message}</h1>
 		<p>Whoops. Seems like that took a wrong turn.</p>
-		<button class="btn variant-filled-primary" onclick={preventDefault(() => goto('/'))}
-			>Main Page</button
-		>
+		<button class="btn variant-filled-primary" onclick={() => goto('/')}>Back</button>
 		<div class="flex justify-center">
 			<ErrorBot height="60vh" />
 		</div>
