93e769623a43c7f38a4ad049b7aac8188b9b2f09
Stefan Poindl <stefan@tohuwabohu.io>
2025-02-23 10:20:36 +0100

    adapt vitest config for coverage on failure

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    setupFiles: ['./tests/__mocks__/skeletonProxy.ts']
+    setupFiles: ['./tests/__mocks__/skeletonProxy.ts'],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   }
 });
