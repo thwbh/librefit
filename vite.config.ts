aefd58468eb1d669cb31e4171ac32755ea481c14
Stefan Poindl <stefan@tohuwabohu.io>
2025-09-04 16:34:44 +0200

    try to reduce chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
     sveltekit(),
     tsconfigPaths(),
   ],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild'
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
