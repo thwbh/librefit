2b0a71583122daa2fc79a85c54b85c91bd9745bd
Stefan Poindl <stefan@tohuwabohu.io>
2025-12-02 11:39:16 +0100

    remove custom linker setting for CI

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,11 +12,12 @@ fn main() {
 
 fn setup_android_llvm() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
+
+    env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
 
     if target_os == "android" {
-        let android_ndk_home = env::var("NDK_HOME").expect("NDK_HOME not set");
-        let build_os = match env::consts::OS {
+        env::var("NDK_HOME").expect("NDK_HOME not set");
+        match env::consts::OS {
             "linux" => "linux",
             "macos" => "darwin",
             "windows" => "windows",
@@ -24,22 +25,5 @@ fn setup_android_llvm() {
                 "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
             ),
         };
-        const DEFAULT_CLANG_VERSION: &str = "19";
-        let clang_version =
-            env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
-
-        // Map Rust target arch to Android arch naming
-        let android_arch = match target_arch.as_str() {
-            "aarch64" => "aarch64",
-            "arm" => "armv7a",
-            "x86" => "i686",
-            "x86_64" => "x86_64",
-            _ => panic!("Unsupported Android architecture: {}", target_arch),
-        };
-
-        let linker = format!(
-            "{android_ndk_home}/toolchains/llvm/prebuilt/{build_os}-x86_64/lib/clang/{clang_version}/lib/linux/libclang_rt.builtins-{android_arch}-android.a");
-
-        println!("cargo:rustc-link-arg={linker}");
     }
 }
